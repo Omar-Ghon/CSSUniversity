56a9e29d6d452f5dd26108980b654db6b874265d
Omar-Ghon <o.ghoneim04@gmail.com>
2024-07-06 13:17:33 +0400

    Finished designing events page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
-const year = document.getElementById("year")
-const thisYear = new Date().getFullYear()
-year.setAttribute("datetime", thisYear)
-year.textContent = thisYear
+document.addEventListener('DOMContentLoaded', (event) => {
+    const yearElements = document.querySelectorAll(".year");
+    const thisYear = new Date().getFullYear();
+    yearElements.forEach(year => {
+        year.setAttribute("datetime", thisYear);
+        year.textContent = thisYear;
+    });
+});
